7bb1bb326f88acd66a3334079103b78c4ae00da9
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2023-11-12 17:04:30 +0530

    [ENHANCE] ThemeToggle

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,18 +1,18 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
-import { MdLightMode } from "react-icons/md";
-import { MdDarkMode } from "react-icons/md";
+'use client';
+import React, { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import { MdOutlineLightMode } from 'react-icons/md';
+import { MdOutlineDarkMode } from 'react-icons/md';
 
 const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
 
   const handleChange = () => {
-    if (theme === "dark") {
-      setTheme("light");
+    if (theme === 'dark') {
+      setTheme('light');
     } else {
-      setTheme("dark");
+      setTheme('dark');
     }
   };
   useEffect(() => {
@@ -26,16 +26,16 @@ const ThemeSwitcher = () => {
     <>
       <button
         onClick={handleChange}
-        className="absolute px-4 py-2 text-2xl text-white transition-all duration-100 bg-gray-800 rounded-lg dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 dark:text-gray-800 md:text-4xl bottom-2 dark:hidden"
+        className="absolute text-black transition-all duration-100 rounded-lg dark:text-gray-800 md:text-2xl bottom-4 dark:hidden"
         data-hide-on-theme="light"
       >
-        <MdDarkMode />
+        <MdOutlineDarkMode />
       </button>
       <button
         onClick={handleChange}
-        className="absolute px-4 py-2 text-2xl text-white transition-all duration-100 bg-gray-800 rounded-lg dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 dark:text-gray-800 md:text-4xl bottom-2 hidden dark:block"
+        className="absolute hidden text-white transition-all duration-100 rounded-lg md:text-2xl bottom-4 dark:block"
       >
-        <MdLightMode />
+        <MdOutlineLightMode />
       </button>
     </>
   );
