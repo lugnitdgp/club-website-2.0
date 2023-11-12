847a202e918c8df1fb2ea6a3d424fef00b74c5d7
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2023-11-12 16:49:19 +0530

    [FIX] Navbar Links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 import {
   add_square_light,
   calendar,
@@ -9,34 +9,45 @@ import {
   video_light,
   logo,
   notebook,
-} from "../../public/assets";
-import ThemeSwitcher from "./ThemeSwitcher";
+} from '../../public/assets';
+import ThemeSwitcher from './ThemeSwitcher';
 
 type Props = {};
 const icons = [
-  { path: "/video", icon: video_light },
-  { path: "/calendar", icon: calendar },
-  { path: "/edit", icon: edit_light },
-  { path: "/group", icon: group },
-  { path: "/notebook", icon: notebook },
-  { path: "/motoboard_light", icon: mortarboard_light },
-  { path: "/edit", icon: add_square_light },
+  { path: '/events', icon: video_light, name: 'Events' },
+  { path: '/timeline', icon: calendar, name: 'Timeline' },
+  { path: '/blogs', icon: edit_light, name: 'Blogs' },
+  { path: '/members', icon: group, name: 'Members' },
 ];
 function Navbar({}: Props) {
   return (
     <>
-      <nav className="bg-pinkSecondary dark:bg-surfaceDark h-screen  px-4 fixed flex flex-col items-center justify-center">
+      <nav className="fixed flex flex-col items-center justify-center h-screen px-4 bg-pinkSecondary dark:bg-surfaceDark">
         <div className="flex flex-col items-center space-y-4">
-          <div className="absolute top-5 mx-auto">
+          <div className="absolute mx-auto top-5">
             <Link href="/">
               <Image src={logo} alt="logo" width={45} height={45} />
             </Link>
           </div>
           {icons.map((icon, index) => {
             return (
-              <Link href={icon.path} key={index} className=" py-2">
-                <Image src={icon.icon} alt="video" className="dark:invert" width={35} height={35} />
-              </Link>
+              <div
+                className="relative flex flex-col items-start group"
+                key={index}
+              >
+                <Link href={icon.path} key={index} className="py-2 ">
+                  <Image
+                    src={icon.icon}
+                    alt="Navbar icon"
+                    className="dark:invert"
+                    width={35}
+                    height={35}
+                  />
+                </Link>
+                <span className="absolute px-1 m-4 mx-auto text-sm text-gray-100 transition-opacity -translate-x-1/2 translate-y-full bg-gray-800 rounded-md opacity-0 left-full group-hover:opacity-100">
+                  {icon.name}
+                </span>
+              </div>
             );
           })}
         </div>
