6026d9ea664113f1882373bdfdc2f2a8ad48ff09
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2023-12-07 13:48:34 +0530

    [FIX] Member cards

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -1,46 +1,48 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import { email, facebook, github } from "../../public/assets";
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { email, facebook, github } from '../../public/assets';
 
 const MemberCard = ({ memberObj, index }: any) => {
   return (
     <>
       <div className=" flex w-max mt-[90px]">
-        <div className="flex justify-center ml-20 mt-[-80px] absolute w-[153px] h-[153px]">
-          <img
-            src={memberObj.image}
-            alt=""
-            className="object-contain rounded-full w-[153px] h-[153px]"
-          />
-        </div>
         <div
           className={
             index % 2 === 0
-              ? "text-center bg-yellowPrimary font-medium text-3xl w-[295px] h-[253px] "
-              : "text-center bg-primaryContainer font-medium text-3xl w-[295px] h-[253px] "
+              ? 'text-center relative bg-yellowPrimary font-medium text-3xl w-[295px] h-[253px] rounded-md px-2'
+              : 'text-center relative bg-primaryContainer font-medium text-3xl w-[295px] h-[253px] rounded-md px-2'
           }
         >
-          <p className="text-center font-medium h-14 text-3xl mt-24">
-            {memberObj.first_name + " " + memberObj.last_name}
+          <div className="absolute left-0 right-0 flex justify-center -top-16 ">
+            <Image
+              src={memberObj.image}
+              alt="member Image"
+              width={100}
+              height={100}
+              className="object-contain rounded-full w-[153px] h-[153px]"
+            />
+          </div>
+          <p className="mt-16 text-lg text-center truncate ">
+            {memberObj.first_name + ' ' + memberObj.last_name}
           </p>
           <div>
-            <p className="text-center font-normal text-xl m-2">
-              {memberObj.bio}
+            <p className="m-2 text-sm font-normal text-center truncate h-9 text-ellipsis">
+              {memberObj?.bio?.length > 0 ? memberObj.bio : 'GLUG Member'}
             </p>
           </div>
-          <div className="flex flex-row h-8 mt-10 justify-center ">
-            <div id="image" className="rounded-20xl m-6 justify-center">
+          <div className="flex flex-row justify-center h-8 ">
+            <div id="image" className="justify-center m-6 rounded-20xl">
               <a href={memberObj.facebook_link}>
                 <Image src={facebook} alt="" className="object-contain" />
               </a>
             </div>
-            <div id="image" className="rounded-20xl m-6">
+            <div id="image" className="m-6 rounded-20xl">
               <a href={memberObj.reddit_link}>
                 <Image src={email} alt="" className="object-contain" />
               </a>
             </div>
-            <div id="image" className="rounded-20xl m-6 my-">
+            <div id="image" className="m-6 rounded-20xl my-">
               <a href={memberObj.linkedin_link}>
                 <Image src={github} alt="" className="object-contain" />
               </a>
